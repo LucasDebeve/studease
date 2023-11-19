e6b85edb35394c39dde5401841e0aeabda03f665
Justin PINGARD <justin.pingard@etudiant.univ-reims.fr>
2023-11-20 00:09:54 +0100

    début tests insertions-professionnelles

--- tests/Controller/InsertionsProfessionnellesController/indexCest.php
+++ tests/Controller/InsertionsProfessionnellesController/indexCest.php
@@ -5,14 +5,31 @@ namespace App\Tests\Controller\InsertionsProfessionnellesController;
 
 use App\Tests\Support\ControllerTester;
 
-class indexCest
+class IndexCest
 {
-    public function _before(ControllerTester $I)
+    public function insertionsProfessionnelles(ControllerTester $I): void
     {
+        // création des données pour test via la Forge à ajouter plus tard
+
+        // affichage de la page
+
+        $I->amOnPage('/insertions-professionnelles');
+        $I->seeResponseCodeIs(200);
+        $I->seeInTitle('Stages et Alternances disponible');
+        $I->see('Stages et Alternances disponible', 'h1');
+        $I->seeCurrentRouteIs('app_insertions_professionnelles');
+
+        // test pour la vérification des liens de chaques éléments de la liste
+
+        // $I->click('');
+        // $I->seeResponseCodeIs(200);
+        // $I->seeCurrentRouteIs(app_detail_insertions_professionnelles);
     }
 
-    // tests
-    public function tryToTest(ControllerTester $I)
+    public function search(ControllerTester $I): void
     {
+        // création des données pour test via la Forge à ajouter plus tard
+
+        // tests pour les détails des stages
     }
 }
