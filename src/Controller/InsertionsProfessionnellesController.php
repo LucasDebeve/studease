c9c42abd1905db219beefc1712bcce3aff49127f
Axel COUDROT <axel.coudrot@free.fr>
2023-11-21 20:26:03 +0100

    Correction de routes et ajout de la possibilité de consulter la page d'une InsertionsProfessionnelles

--- src/Controller/InsertionsProfessionnellesController.php
+++ src/Controller/InsertionsProfessionnellesController.php
@@ -6,15 +6,21 @@ use App\Repository\InsertionProfessionnelleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\InsertionProfessionnelle;
 
 class InsertionsProfessionnellesController extends AbstractController
 {
-
-    #[Route('/test', name: 'app_insertions_professionnelles')]
+    #[Route('/insertions_professionnelles', name: 'app_insertions_professionnelles')]
     public function index(InsertionProfessionnelleRepository $repository): Response
     {
-        $insertions = $repository->findBy([], ['titre' => 'ASC', 'typePro' => 'ASC']);
+        $insertions = $repository->search();
 
         return $this->render('insertions_professionnelles/index.html.twig', ['insertions' => $insertions]);
+    }
+
+    #[Route('/insertions_professionnelles/{id}', name: 'app_insertions_professionnelles_id')]
+    public function show(InsertionProfessionnelle $insertion): Response
+    {
+        return $this->render('insertions_professionnelles/show.html.twig', ['insertion' => $insertion]);
     }
 }
