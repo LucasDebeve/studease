a29800027e4e73d364d70ae6b170eee2a867e67d
Axel COUDROT <axel.coudrot@free.fr>
2023-12-10 13:13:30 +0100

    Ajout du filtrage pour la date de début (non fonctionnel), et de la durée

--- assets/insertion.js
+++ assets/insertion.js
@@ -34,7 +34,8 @@ function setAddFilterInsertionCallback(insertions) {
     const intituleInsertion = document.querySelector("input.input.intitule_insertion");
     const typeContrat = document.querySelector("select.input.type_contrat_insertion");
     const dateDebPre = document.querySelector("input.input.date_debut_avant_insertion");
-    const dateDebPost = document.querySelector("input.input.date_debut_apres_insertion")
+    const dateDebPost = document.querySelector("input.input.date_debut_apres_insertion");
+    const duree = document.querySelector("input.input.duree_insertion")
 
     intituleInsertion.addEventListener("keyup", function () {
         const intitule = document.getElementById("intitule").value.toString().toUpperCase();
@@ -82,10 +83,65 @@ function setAddFilterInsertionCallback(insertions) {
         });
     });
 
+    // ne fonctionne pas pour le moment
     dateDebPre.addEventListener("change",()=>{
-        const dateDebBef= document.getElementById("date_debut_avant");
-        const dateDebAft = document.getElementById("date_debut_apres");
-        console.log(dateDebBef);
-        console.log(dateDebAft);
+        const dateDebBef= new Date(document.getElementById("date_debut_avant").value);
+        const dateDebAft = new Date(document.getElementById("date_debut_apres").value);
+        dateDebAft.setHours(0,0,0,0);
+        dateDebBef.setHours(0,0,0,0);
+        insertionsList.forEach(function (insertion) {
+            let dateDeb= new Date(insertion.dateDeb);
+            dateDeb.setHours(0,0,0,0);
+            if(dateDebBef <= dateDeb <= dateDebAft){
+                if (!insertions.contains(insertion['query'])) {
+                    insertions.appendChild(insertion['query']);
+                }
+            }
+            else{
+                if (insertions.contains(insertion['query'])) {
+                    insertions.removeChild(insertion['query']);
+                }
+            }
+        });
+    });
+
+    duree.addEventListener("keyup", ()=>{
+        const dureeInsertion = parseInt(document.getElementById("duree").value,10);
+        insertionsList.forEach(function (insertion) {
+            if(insertion.duree === dureeInsertion){
+                if (!insertions.contains(insertion['query'])) {
+                    insertions.appendChild(insertion['query']);
+                }
+            } else{
+                if (insertions.contains(insertion['query'])) {
+                    insertions.removeChild(insertion['query']);
+                }
+            }
+            if(!dureeInsertion){
+                if (!insertions.contains(insertion['query'])) {
+                    insertions.appendChild(insertion['query']);
+                }
+            }
+        });
+    });
+
+    duree.addEventListener("change", ()=>{
+        const dureeInsertion = parseInt(document.getElementById("duree").value,10);
+        insertionsList.forEach(function (insertion) {
+            if(insertion.duree === dureeInsertion){
+                if (!insertions.contains(insertion['query'])) {
+                    insertions.appendChild(insertion['query']);
+                }
+            } else{
+                if (insertions.contains(insertion['query'])) {
+                    insertions.removeChild(insertion['query']);
+                }
+            }
+            if(!dureeInsertion){
+                if (!insertions.contains(insertion['query'])) {
+                    insertions.appendChild(insertion['query']);
+                }
+            }
+        });
     });
 }
