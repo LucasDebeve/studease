40846067f80e5a202244e0902917bc60b7f0d82e
Nathan Eullaffroy <nathan.eullaffroy@etudiant.univ-reims.fr>
2023-12-17 19:15:32 +0100

    Création de l'action update du Controller

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -1,9 +1,30 @@
 <?php
-declare(strict_types=1);
 
 namespace App\Controller;
 
-class EntrepriseController
-{
+use App\Entity\Candidature;
+use App\Form\EntrepriseCandidatureType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+class EntrepriseController extends AbstractController
+{
+    #[Route('/candidature/{id}/update', name: 'app_candidature_update', requirements: ['id' => '\d+'])]
+    public function updateStatut(Request $request, EntityManagerInterface $entityManager, Candidature $candidature): Response
+    {
+        $form = $this->createForm(EntrepriseCandidatureType::class, $candidature);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+        }
+
+        return $this->render('entreprise/modifier_candidature.html.twig', [
+            'form' => $form->createView(),
+            'candidature' => $candidature,
+        ]);
+    }
 }
