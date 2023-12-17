758b07e6aed032890f25b9728345b6e39e3f740a
Nathan Eullaffroy <nathan.eullaffroy@etudiant.univ-reims.fr>
2023-12-17 19:03:19 +0100

    BuilderForm configuré

--- src/Form/EntrepriseCandidatureType.php
+++ src/Form/EntrepriseCandidatureType.php
@@ -3,9 +3,21 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class EntrepriseCandidatureType extends AbstractType
 {
-
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'En attente' => 'en_attente',
+                    'Accepté' => "accepte",
+                    'Refusé' => 'refuse',
+                ],
+                'label' => 'Statut de la candidature',
+            ]);
+    }
 }
