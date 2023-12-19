865eb0de4961d0bb6453729cba489f7d1d8f07e3
JACQUEMAIN Paco <paco.jacquemain@etudiant.univ-reims.fr>
2023-12-19 22:40:40 +0100

    Création formulaire de vérification d'utilisateur

---
+++ src/Form/VerifyUserFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VerifyUserFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('confirm', SubmitType::class)
+            ->add('deny', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
