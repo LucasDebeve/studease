3885695fa084a7435f88e9940c76eb5156df4a18
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2023-12-22 14:28:19 +0100

    Petite Modif

--- src/DataFixtures/InsertionsProfessionnellesFixtures.php
+++ src/DataFixtures/InsertionsProfessionnellesFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\FormationFactory;
 use App\Factory\InsertionProfessionnelleFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -12,16 +11,16 @@ class InsertionsProfessionnellesFixtures extends Fixture implements DependentFix
 {
     public function load(ObjectManager $manager): void
     {
-        $formationNames = json_decode(file_get_contents(__DIR__.'/Data/insertions.json'), true);
+        $formationNames = json_decode(file_get_contents(__DIR__.'/Data/insertions.json'), true)['stages'];
         foreach ($formationNames as $formationName) {
-            var_dump($formationName[0]);
             InsertionProfessionnelleFactory::createOne(
                 [
-                    'titre' => $formationName[0]['titre'],
+                    'titre' => $formationName['titre'],
                 ]
             );
         }
     }
+
     public function getDependencies(): array
     {
         return [
