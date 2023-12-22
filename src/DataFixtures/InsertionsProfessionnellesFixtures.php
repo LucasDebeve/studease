dc7aaeaddf0206c0cb3ead9e77ef8fdf0fe3a452
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2023-12-22 14:28:19 +0100

    Petite Modif

--- src/DataFixtures/InsertionsProfessionnellesFixtures.php
+++ src/DataFixtures/InsertionsProfessionnellesFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\FormationFactory;
 use App\Factory\InsertionProfessionnelleFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
