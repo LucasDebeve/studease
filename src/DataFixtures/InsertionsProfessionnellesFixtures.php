9c58fb51c93eff4e8354002db597960bb89cb492
ping0010 <justin.pingard@etudiant.univ-reims.fr>
2024-01-03 22:32:19 +0100

    ajout de fixtures de test et modif pour lier les insertions a une entreprise

--- src/DataFixtures/InsertionsProfessionnellesFixtures.php
+++ src/DataFixtures/InsertionsProfessionnellesFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\InsertionProfessionnelleFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -11,6 +12,7 @@ class InsertionsProfessionnellesFixtures extends Fixture implements DependentFix
 {
     public function load(ObjectManager $manager): void
     {
+        InsertionProfessionnelleFactory::createOne(['company' => UserFactory::random(['id' => 2])]);
         InsertionProfessionnelleFactory::createMany(50);
 
         $manager->flush();
