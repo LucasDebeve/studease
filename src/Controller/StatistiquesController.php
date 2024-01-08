b13addde53c133c46791c774bba5168dcf5e2fdb
debe0033 <lucas.debeve@etudiant.univ-reims.fr>
2024-01-08 11:10:11 +0100

    Ajout des tooltips

--- src/Controller/StatistiquesController.php
+++ src/Controller/StatistiquesController.php
@@ -7,10 +7,12 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class StatistiquesController extends AbstractController
 {
     #[Route('/stats', name: 'app_stats')]
+    // #[IsGranted('ROLE_ADMIN')]
     public function index(InsertionProfessionnelleRepository $insertions_repo, UserRepository $userRepository): Response
     {
         // Nombre de propositions et candidatures par type d'insertion
