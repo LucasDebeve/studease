787939f1ee95bd0c8a2cdf648477d421824f7832
Nathan Eullaffroy <nathan.eullaffroy@etudiant.univ-reims.fr>
2024-01-08 20:17:18 +0100

    Création du formulaire, interrogation sur l'attribut compagnie

--- src/Form/InsertionProType.php
+++ src/Form/InsertionProType.php
@@ -3,7 +3,15 @@
 namespace App\Form;
 
 use App\Entity\InsertionProfessionnelle;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +20,31 @@ class InsertionProType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('dateDeb')
-            ->add('dateFin')
-            ->add('descInsPro')
-            ->add('teletravail')
-            ->add('titre')
-            ->add('revenus')
-            ->add('typePro')
-            ->add('duree')
-            ->add('company')
+            ->add('dateDeb', DateType::class, [
+                'empty_data' => ' ',
+            ])
+            ->add('dateFin', DateType::class, [
+                'empty_data' => ' ',
+            ])
+            ->add('descInsPro', TextareaType::class, [
+                'empty_data' => ' ',
+            ])
+            ->add('teletravail', CheckboxType::class, [
+                'label' => 'Actif',
+                'required' => false,
+            ])
+            ->add('titre', TextType::class, [
+                'empty_data' => ' ',
+            ])
+            ->add('revenus', MoneyType::class, [
+                'empty_data' => ' ',
+            ])
+            ->add('typePro', IntegerType::class, [
+                'empty_data' => ' ',
+            ])
+            ->add('duree', DateIntervalType::class, [
+                'empty_data' => ' ',
+            ])
         ;
     }
 
