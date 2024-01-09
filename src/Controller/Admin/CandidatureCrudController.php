f919f6d6e38660f994278f5bd565ea6d3b684e3a
debe0033 <lucas.debeve@etudiant.univ-reims.fr>
2024-01-09 12:29:42 +0100

    Filtres des candidatures

--- src/Controller/Admin/CandidatureCrudController.php
+++ src/Controller/Admin/CandidatureCrudController.php
@@ -3,13 +3,19 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Candidature;
+use App\Repository\InsertionProfessionnelleRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Orm\EntityRepositoryInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class CandidatureCrudController extends AbstractCrudController
 {
@@ -70,5 +76,39 @@ class CandidatureCrudController extends AbstractCrudController
     {
         $entityInstance->setDate(new \DateTime('now'));
         parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(
+                EntityFilter::new('candidat')
+                    ->setFormTypeOption('value_type_options.query_builder', function (UserRepository $er) {
+                        return $er->createQueryBuilder('u')
+                            ->andWhere('u.roles LIKE :role')
+                            ->setParameter('role', '%ROLE_STUDENT%')
+                            ->orderBy('u.name', 'ASC')
+                            ->addOrderBy('u.firstname', 'ASC');
+                    })
+                ->setFormTypeOption('value_type_options.choice_label', function ($candidat) {
+                    return $candidat->getFirstname().' '.$candidat->getName();
+                })
+            )
+            ->add(EntityFilter::new('insertion_professionnelle')
+                ->setFormTypeOption('value_type_options.query_builder', function (InsertionProfessionnelleRepository $er) {
+                    return $er->createQueryBuilder('i')
+                        ->orderBy('i.titre', 'ASC');
+                })
+                ->setFormTypeOption('value_type_options.choice_label', function ($insertion) {
+                    return $insertion->getId().' - '.$insertion->getTitre();
+                }))
+            ->add('date')
+            ->add(ChoiceFilter::new('statut')
+                ->canSelectMultiple(true)
+                ->setChoices([
+                    'En attente' => 0,
+                    'Acceptée' => 1,
+                    'Refusée' => 2,
+                ]));
     }
 }
