a42873f02bf1744f320083acf7fb8bcf232a1c5b
Nathan Eullaffroy <nathan.eullaffroy@etudiant.univ-reims.fr>
2024-01-09 19:59:04 +0100

    Affichage du choix stage ou alternance dans le form

--- src/Form/InsertionProType.php
+++ src/Form/InsertionProType.php
@@ -6,13 +6,15 @@ use App\Entity\InsertionProfessionnelle;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class InsertionProType extends AbstractType
@@ -21,6 +23,7 @@ class InsertionProType extends AbstractType
     {
         $builder
             ->add('dateDeb', DateType::class, [
+                'required' => false,
                 'empty_data' => ' ',
             ])
             ->add('dateFin', DateType::class, [
@@ -40,10 +43,14 @@ class InsertionProType extends AbstractType
                 'empty_data' => ' ',
                 'currency' => 'EUR',
             ])
-            ->add('typePro', IntegerType::class, [
-                'empty_data' => ' ',
+            ->add('typePro', ChoiceType::class, [
+                'choices' => [
+                    'Stage' => 1,
+                    'Alternance' => 2,
+                ],
             ])
             ->add('duree', IntegerType::class, [
+                'required' => false,
                 'empty_data' => ' ',
             ])
             ->add('company', EntityType::class, [
