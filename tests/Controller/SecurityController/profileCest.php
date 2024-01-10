fabd6725a7db79b9c0a71c8b70ae46b6d2bc2665
ping0010 <justin.pingard@etudiant.univ-reims.fr>
2024-01-10 19:31:58 +0100

    test redirection au profil

---
+++ tests/Controller/SecurityController/profileCest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller\SecurityController;
+
+use App\Factory\UserFactory;
+use App\Tests\Support\ControllerTester;
+
+class profileCest
+{
+    public function redirectionToProfile(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne(['roles' => ['ROLE_STUDENT']]);
+        $user = $user->object();
+        $I->amLoggedInAs($user);
+        $I->amOnPage('/profile');
+        $I->seeCurrentUrlEquals('/profile/'.$user->getId());
+    }
+}
