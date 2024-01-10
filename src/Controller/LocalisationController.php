f727a8e514c2b1df6c31bef15729829e5f98600a
debe0033 <lucas.debeve@etudiant.univ-reims.fr>
2024-01-10 18:44:26 +0100

    Modification de la Localisation

--- src/Controller/LocalisationController.php
+++ src/Controller/LocalisationController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Localisation;
+use App\Form\LocalisationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -20,5 +23,24 @@ class LocalisationController extends AbstractController
         return $this->render('localisation/index.html.twig', [
             'localisation' => $localisation,
         ]);
+    }
+
+    #[Route('/localisation/{id}/update', name: 'app_localisation_update')]
+    public function update(EntityManagerInterface $entityManager, Localisation $id, Request $request)
+    {
+        $form = $this->createForm(LocalisationType::class, $id);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $localisation = $form->getData();
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_localisation_insertions', ['id' => $localisation->getId()]);
+        }
+
+        $this->addFlash('success', 'Localisation modifiée.');
+
+        return $this->render('localisation/update.html.twig', ['localisation' => $id, 'form' => $form]);
     }
 }
