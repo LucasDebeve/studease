bd0b464abde732bf1ee2d20a177af28bce93e668
Axel COUDROT <axel.coudrot@free.fr>
2024-01-10 22:56:06 +0100

    ajout de l'affichage des violation du validator lors de l'inscription

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,49 +12,67 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
+        $errors_password = $validator->validate($form->get('plainPassword')->getData(), [
+            new Length(['min' => 6]),
+            new NotBlank(),
+        ]);
+        $parameters = [
+            'registrationForm' => $form->createView(), ];
+        $errors_email = $validator->validate($user);
+        if (0 == count($errors_password) && 0 == count($errors_email)) {
+            if ($form->isSubmitted() && $form->isValid()) {
+                // encode the plain password
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+                switch ($form->get('tpUser')->getData()) {
+                    case 1:
+                        $user->setRoles(['ROLE_STUDENT']);
+                        break;
+                    case 2:
+                        $user->setRoles(['ROLE_COMPANY']);
+                        break;
+                    default:
+                        $user->setRoles(['ROLE_USER']);
+                }
+                $user->setIsVerified(false);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
+                $entityManager->persist($user);
+                $entityManager->flush();
+                // do anything else you need here, like send an email
+
+                return $userAuthenticator->authenticateUser(
                     $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-            switch ($form->get('tpUser')->getData()) {
-                case 1:
-                    $user->setRoles(['ROLE_STUDENT']);
-                    break;
-                case 2:
-                    $user->setRoles(['ROLE_COMPANY']);
-                    break;
-                default:
-                    $user->setRoles(['ROLE_USER']);
+                    $authenticator,
+                    $request
+                );
             }
-            $user->setIsVerified(false);
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
-
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
-            );
+        } else {
+            if (0 < count($errors_email)) {
+                $errors_email = $errors_email->get(0);
+                $parameters['errors_email'] = $errors_email;
+            }
+            if (0 < count($errors_password)) {
+                $errors_password = $errors_password->get(0);
+                $parameters['errors_password'] = $errors_password;
+            }
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
+        return $this->render('registration/register.html.twig', $parameters);
     }
 }
