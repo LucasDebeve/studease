7970a8dc886b7e24382ab808d63796901094a557
Nathan Eullaffroy <nathan.eullaffroy@etudiant.univ-reims.fr>
2024-01-10 21:15:46 +0100

    Gestion conflit #3

--- src/Form/InsertionProType.php
+++ src/Form/InsertionProType.php
@@ -3,18 +3,8 @@
 namespace App\Form;
 
 use App\Entity\InsertionProfessionnelle;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class InsertionProType extends AbstractType
@@ -22,42 +12,16 @@ class InsertionProType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('dateDeb', DateType::class, [
-                'required' => false,
-            ])
-            ->add('dateFin', DateType::class, [
-                'required' => false,
-            ])
-            ->add('descInsPro', TextareaType::class, [
-                'required' => false,
-                'empty_data' => ' ',
-            ])
-            ->add('teletravail', CheckboxType::class, [
-                'required' => false,
-            ])
-            ->add('titre', TextType::class, [
-                'required' => false,
-                'empty_data' => ' ',
-            ])
-            ->add('revenus', MoneyType::class, [
-                'required' => false,
-            ])
-            ->add('typePro', ChoiceType::class, [
-                'choices' => [
-                    'Stage' => 1,
-                    'Alternance' => 2,
-                ],
-            ])
-            ->add('duree', IntegerType::class, [
-                'required' => false,
-                'empty_data' => ' ',
-                'mapped' => false,
-            ])
-            ->add('company', EntityType::class, [
-                'class' => 'App\Entity\User',
-                'choice_label' => 'name',
-                'disabled' => true,
-            ]);
+            ->add('dateDeb')
+            ->add('dateFin')
+            ->add('descInsPro')
+            ->add('teletravail')
+            ->add('titre')
+            ->add('revenus')
+            ->add('typePro')
+            ->add('duree')
+            ->add('company')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
