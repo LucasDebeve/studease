4c3fdfa023538edb46af308f61fcdc9f75037db8
ping0010 <justin.pingard@etudiant.univ-reims.fr>
2024-01-10 19:55:51 +0100

    tests acces pages de suppresion d'insertion

---
+++ tests/Controller/InsertionsProfessionnellesController/DeleteCest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Controller\InsertionsProfessionnellesController;
+
+use App\Factory\InsertionProfessionnelleFactory;
+use App\Factory\UserFactory;
+use App\Tests\Support\ControllerTester;
+use Codeception\Util\HttpCode;
+
+class DeleteCest
+{
+    public function form(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne(['tpUser' => 2, 'isVerified' => true]);
+        InsertionProfessionnelleFactory::createOne();
+        $user = $user->object();
+        $I->amLoggedInAs($user);
+        $I->amOnPage('/insertions/1/delete');
+        $I->seeResponseCodeIs(HttpCode::OK);
+    }
+}
