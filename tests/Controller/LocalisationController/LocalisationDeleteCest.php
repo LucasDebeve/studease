8ddaf8483189993556c91d1aeab26fe7db0a73a0
ping0010 <justin.pingard@etudiant.univ-reims.fr>
2024-01-10 21:49:26 +0100

    test affichage de formulaire (page suppression de localisation)

---
+++ tests/Controller/LocalisationController/LocalisationDeleteCest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Tests\Controller\LocalisationController;
+
+use App\Factory\LocalisationFactory;
+use App\Factory\UserFactory;
+use App\Tests\Support\ControllerTester;
+use Codeception\Util\HttpCode;
+
+class LocalisationDeleteCest
+{
+    public function form(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne(['tpUser' => 2, 'isVerified' => true, 'roles' => ['ROLE_COMPANY']]);
+        LocalisationFactory::createOne(['entreprise' => $user]);
+        $user = $user->object();
+        $I->amLoggedInAs($user);
+        $I->amOnPage('/localisation/1/delete');
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeInTitle('Suppression de la localisation');
+        $I->see('Suppression de la Localisation suivante', 'h1');
+    }
+}
